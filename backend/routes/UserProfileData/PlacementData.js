9d83316bc0c7b66fae15ad46e0cbe74deafcae25
Sangam Arora <arorasam2109@gmail.com>
2024-03-01 21:54:59 +0530

    backend get route files

--- backend/routes/UserProfileData/PlacementData.js
+++ backend/routes/UserProfileData/PlacementData.js
@@ -33,5 +33,20 @@ router.post('/', async (req, res) => {
         res.status(400).json({ message: error.message });
     }
 });
+router.get('/:urn', async (req, res) => {
+    try {
+        const urn = req.params.urn;
+        const userInfo = await SignUpdata.findOne({ urn: urn }).populate('tr101');
+
+        if (!userInfo) {
+            return res.status(404).json({ message: 'UserInfo not found' });
+        }
+
+        // Respond with the user information
+        res.status(200).json({ success: true, data: userInfo.placementData });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
 
 module.exports = router;
