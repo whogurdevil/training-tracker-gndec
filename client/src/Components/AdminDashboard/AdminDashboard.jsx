99c1d69d75531220a5a5ea96903bf4377bd30d3c
Sangam Arora <arorasam2109@gmail.com>
2024-03-02 15:33:39 +0530

    added admin dashboard

--- client/src/Components/AdminDashboard/AdminDashboard.jsx
+++ client/src/Components/AdminDashboard/AdminDashboard.jsx
@@ -1,8 +1,104 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import axios from 'axios';
-import { Card, CardContent, Typography, Button, Modal, Box, Grid } from '@mui/material';
+import { Card, Button, Modal, Box, Typography, Grid, MenuItem, Select, FormControl, InputLabel } from '@mui/material';
+import { MaterialReactTable, useMaterialReactTable } from 'material-react-table';
 
-const UserCard = ({ user }) => {
+const AdminForm = () => {
+    const [users, setUsers] = useState([]);
+    const [selectedBatch, setSelectedBatch] = useState('');
+    const [selectedBranch, setSelectedBranch] = useState('');
+    const [selectedTraining, setSelectedTraining] = useState('');
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                const token = localStorage.getItem('authtoken');
+                const response = await axios.get('http://localhost:8000/api/users/getallusers/', {
+                    headers: {
+                        "auth-token": token // Include the authentication token in the request headers
+                    }
+                });
+                const filteredUsers = response.data.data
+                    .filter(user => user.role === 'user')
+                    .sort((a, b) => a.urn - b.urn);
+                setUsers(filteredUsers);
+               
+            } catch (error) {
+                console.error('Error fetching users:', error);
+            }
+        };
+
+        fetchUsers();
+    }, []);
+
+    const filteredUsers = useMemo(() => {
+        let filteredData = [...users];
+
+        if (selectedBatch) {
+            filteredData = filteredData.filter(user => user.userInfo.batch === selectedBatch);
+        }
+
+        if (selectedBranch) {
+            filteredData = filteredData.filter(user => user.userInfo.branch === selectedBranch);
+        }
+
+        if (selectedTraining) {
+            filteredData = filteredData.filter(user => user[selectedTraining]);
+        }
+
+        return filteredData;
+    }, [users, selectedBatch, selectedBranch, selectedTraining]);
+
+    const columns = useMemo(
+        () => {
+            let customColumns = [
+                { accessorKey: "urn", header: "URN" },
+                { accessorKey: "userInfo.Name", header: "Name" },
+                { accessorKey: "userInfo.crn", header: "CRN" }
+            ];
+
+            if (selectedTraining === 'placementData') {
+                customColumns.push(
+                    { accessorKey: "placementData.package", header: "Package" },
+                    { accessorKey: "placementData.appointmentLetter", header: "Appointment Letter" },
+                    { accessorKey: "placementData.appointmentDate", header: "Appointment Date" },
+                    { accessorKey: "placementData.company", header: "Company" },
+                    { accessorKey: `${selectedTraining}.lock`, header: "Verified" }
+                );
+            } else if (selectedTraining) {
+                customColumns.push(
+                    { accessorKey: selectedTraining + ".technology", header: "Technology" },
+                    { accessorKey: selectedTraining + ".certificate", header: "Certificate" },
+                    { accessorKey: selectedTraining + ".projectName", header: "Project Name" },
+                    { accessorKey: selectedTraining + ".type", header: "Type" }
+                );
+            }
+
+            
+            customColumns.push({
+                accessorKey: `${selectedTraining}.lock`,
+                header: "Verified",
+                renderCell: ({ row }) => (
+                    <Typography>
+                        {console.log(row.original)}
+                        {row.original[selectedTraining] && row.original[selectedTraining].lock ? 'True' : 'False'}
+                   
+                    </Typography>
+                )
+            }
+);
+            
+
+            return customColumns;
+        },
+        [selectedTraining]
+    );
+
+    const table = useMaterialReactTable({
+        data: filteredUsers,
+        columns
+    });
+
     const [open, setOpen] = useState(false);
     const [modalContent, setModalContent] = useState(null);
 
@@ -15,46 +111,60 @@ const UserCard = ({ user }) => {
         setOpen(false);
     };
 
+    const handleBatchChange = (event) => {
+        setSelectedBatch(event.target.value);
+    };
+
+    const handleBranchChange = (event) => {
+        setSelectedBranch(event.target.value);
+    };
+
+    const handleTrainingChange = (event) => {
+        setSelectedTraining(event.target.value);
+    };
+
     return (
-        <>
-            <Card variant="outlined" style={{ marginBottom: '50px' }}>
-                <CardContent>
-                    <Typography variant="h5" component="div">
-                        {user.userInfo.Name}
-                    </Typography>
-                    <Typography color="text.secondary">
-                        Branch: {user.userInfo.branch} | URN: {user.urn}
-                    </Typography>
-                    <Grid container spacing={1} style={{ marginTop: '10px' }}>
-                        <Grid item>
-                            <Button variant="contained" onClick={() => handleModalOpen(user.tr101)}>
-                                TR101
-                                {console.log(user.tr101)}
-                            </Button>
-                        </Grid>
-                        <Grid item>
-                            <Button variant="contained" onClick={() => handleModalOpen(user.tr102)}>
-                                TR102
-                            </Button>
-                        </Grid>
-                        <Grid item>
-                            <Button variant="contained" onClick={() => handleModalOpen(user.tr103)}>
-                                TR103
-                            </Button>
-                        </Grid>
-                        <Grid item>
-                            <Button variant="contained" onClick={() => handleModalOpen(user.tr104)}>
-                                TR104
-                            </Button>
-                        </Grid>
-                        <Grid item>
-                            <Button variant="contained" onClick={() => handleModalOpen(user.placementData)}>
-                                Placement Data
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </CardContent>
+        <div style={{ marginTop: '100px', padding: '0 20px' }}>
+            <Grid container spacing={2} justifyContent="space-around">
+                <Grid item style={{  marginBottom: 20 }}>
+                    <FormControl style={{width:200}}>
+                        <InputLabel>Batch</InputLabel>
+                        <Select value={selectedBatch} onChange={handleBatchChange}>
+                            <MenuItem value="">All</MenuItem>
+                            <MenuItem value="2020-2024">2020-2024</MenuItem>
+                            <MenuItem value="2021-2025">2021-2025</MenuItem>
+                            {/* Add other batch options */}
+                        </Select>
+                    </FormControl>
+                </Grid>
+                <Grid item style={{ marginBottom: 20 }}>
+                    <FormControl style={{ width: 200 }}>
+                        <InputLabel>Branch</InputLabel>
+                        <Select value={selectedBranch} onChange={handleBranchChange}>
+                            <MenuItem value="">All</MenuItem>
+                            <MenuItem value="CSE">Computer Science & Engineering</MenuItem>
+                            {/* Add other branch options */}
+                        </Select>
+                    </FormControl>
+                </Grid>
+                <Grid item style={{ marginBottom: 20 }}>
+                    <FormControl style={{ width: 200 }}>
+                        <InputLabel>Training</InputLabel>
+                        <Select value={selectedTraining} onChange={handleTrainingChange}>
+                            <MenuItem value="">All</MenuItem>
+                            <MenuItem value="tr101">Training 101</MenuItem>
+                            <MenuItem value="tr102">Training 102</MenuItem>
+                            <MenuItem value="tr103">Training 103</MenuItem>
+                            <MenuItem value="tr104">Training 104</MenuItem>
+                            <MenuItem value="placementData">Placement Data</MenuItem>
+                        </Select>
+                    </FormControl>
+                </Grid>
+            </Grid>
+            <Card variant="outlined" style={{ marginBottom: '50px'}}>
+                <MaterialReactTable table={table}/>
             </Card>
+
             <Modal open={open} onClose={handleModalClose}>
                 <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', boxShadow: 24, p: 4 }}>
                     {modalContent && (
@@ -70,44 +180,6 @@ const UserCard = ({ user }) => {
                     )}
                 </Box>
             </Modal>
-        </>
-    );
-};
-
-const AdminForm = () => {
-    const [users, setUsers] = useState([]);
-
-    useEffect(() => {
-        const fetchUsers = async () => {
-            try {
-                const token = localStorage.getItem('authtoken');
-                const response = await axios.get('http://localhost:8000/api/users/getallusers/', {
-                    headers: {
-                        "auth-token": token // Include the authentication token in the request headers
-                    }
-                });
-                //   console.log(response.data)
-                const filteredUsers = response.data.data.filter(user => user.role === 'user');
-                setUsers(filteredUsers);
-
-            } catch (error) {
-                console.error('Error fetching users:', error);
-            }
-        };
-
-        fetchUsers();
-    }, []);
-
-
-    return (
-        <div style={{ marginTop: '100px' }}>
-        <div>
-    ADMIN
-        </div>
-            {console.log(users)}
-            {users.map((user) => (
-                <UserCard key={user._id} user={user} />
-            ))}
         </div>
     );
 };
