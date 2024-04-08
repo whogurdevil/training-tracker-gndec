6bda47700946bc1d5725b45e5560d5077be5316b
whogurdevil <gur13dev@gmail.com>
2024-04-08 19:38:10 +0530

    changed genconians text to link

--- client/src/Components/Footer.jsx
+++ client/src/Components/Footer.jsx
@@ -1,13 +1,25 @@
-import { Typography } from "@mui/material";
+import { Typography, Link } from "@mui/material";
 
 const Footer = () => {
-    return (
-      <footer style={{ backgroundColor: '#900000', color: 'white', textAlign: 'center', padding: '20px', position: 'fixed', bottom: 0, left: 0, width: '100%', zIndex:'100' }}>
-        <Typography variant="body1">
-          Made with 🤍 by Genconians
-        </Typography>
-      </footer>
-    );
-  }
+  return (
+    <footer style={{ backgroundColor: '#900000', color: 'white', textAlign: 'center', padding: '20px', position: 'fixed', bottom: 0, left: 0, width: '100%', zIndex: '100' }}>
+      <Typography variant="body1">
+        {'Made with 🤍 by '}
+        <span>
+          <Link
+            // href="/forgotpassword"
+            // variant="body2"
+            sx={{
+              color: 'white',
+              textDecoration: 'underline'
+            }}
+          >
+            { 'Genconians' }
+          </Link>
+        </span>
+      </Typography>
+    </footer>
+  );
+}
 
-  export default Footer
+export default Footer
