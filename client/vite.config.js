5248cac26cac96ebdb186fbdc9950606cbbea942
whogurdevil <gur13dev@gmail.com>
2024-04-17 17:02:23 +0530

    resolved url errors for production

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  server: {
+    proxy: {
+      // Proxy options
+      '/assets': {
+        target: 'http://127.0.0.1:5500/', // Your server URL
+        changeOrigin: true,
+        secure: false, // if you're using HTTPS, set it to true
+        headers: {
+          'Access-Control-Allow-Origin': '*', // or specific origin
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization'
+        }
+      }
+    }
+  }
+});
