64a7dba75cfc3a50456f631b6da5d9796fd7b149
whogurdevil <gur13dev@gmail.com>
2024-04-30 16:57:10 +0530

    db initialization succesfully implemented

--- backend/db.js
+++ backend/db.js
@@ -1,13 +1,137 @@
-const mongoose=require('mongoose');
+const mongoose = require('mongoose');
+const adminControl = require('./models/adminControl');
+const { SignUp } = require('./models/UserInfo');
 require('dotenv').config();
-const db=()=>{
-try {
-    mongoose.connect(process.env.DATABASE);
-    console.log("connected to mongodb successfully")    
-} catch (error) {
-    console.log("Some error occured")
-}
-}
-module.exports =db;
+
+const db = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE);
+        console.log("connected to mongodb successfully")
+
+        const adminControlsCount = await adminControl.countDocuments({});
+        if (adminControlsCount === 0) {
+            await initializeAdminControls();
+        }
+
+        const usersCount = await SignUp.countDocuments({});
+        if (usersCount === 0) {
+            await initializeUsers();
+        }
 
 
+    } catch (error) {
+        console.log("Some error occured", error)
+    }
+}
+async function initializeAdminControls() {
+    try {
+        const adminControls = new adminControl({
+            Training_No: 4,
+            Training1_name: "Training 101",
+            Training2_name: "Training 102",
+            Training3_name: "Training 103",
+            Training4_name: "Training 104",
+            Placement_name: "Placement Data"
+        });
+        await adminControls.save();
+        console.log("Initialized admincontrols collection with a document");
+    } catch (error) {
+        console.error("Error initializing admincontrols collection:", error);
+    }
+}
+async function initializeUsers() {
+    try {
+        const user1 = new SignUp({
+            crn: "Tr000",
+            email: "superadmin@gmail.com",
+            password: "$2b$10$IbUz.xayDPWiNLcbQoIqqOHucE.5EwPQAfeMSx0JC0IUT.pGeJlHG",
+            role: "superadmin",
+            isVerified: true,
+            userInfo: {
+                urn: 1111111,
+                branch: "Computer Science & Enginnering"
+            },
+            tr101: { technology: [], lock: false },
+            tr102: { technology: [], lock: false },
+            tr103: { technology: [], lock: false },
+            tr104: { technology: [], lock: false },
+            placementData: { lock: false }
+        });
+        await user1.save();
+        const user2 = new SignUp({
+            crn: "Tr101",
+            email: "training101@gmail.com",
+            password: "$2b$10$3kZDoENoeP/VPHuEm5xWT.sXKz5dGycptnjtLyd8fBLA9hBoiUPu2",
+            role: "admin",
+            isVerified: true,
+            userInfo: {
+                urn: 1111112,
+                branch: "Computer Science & Enginnering"
+            },
+            tr101: { technology: [], lock: false },
+            tr102: { technology: [], lock: false },
+            tr103: { technology: [], lock: false },
+            tr104: { technology: [], lock: false },
+            placementData: { lock: false }
+        });
+        await user2.save();
+        const user3 = new SignUp({
+            crn: "Tr102",
+            email: "training102@gmail.com",
+            password: "$2b$10$YdkjLd1seFbyt6B9/UPdoeSU.YajF8lNC5KWwz6UZu5aP06jqAj3u",
+            role: "admin",
+            isVerified: true,
+            userInfo: {
+                urn: 1111113,
+                branch: "Computer Science & Enginnering"
+            },
+            tr101: { technology: [], lock: false },
+            tr102: { technology: [], lock: false },
+            tr103: { technology: [], lock: false },
+            tr104: { technology: [], lock: false },
+            placementData: { lock: false }
+        });
+        await user3.save();
+        const user4 = new SignUp({
+            crn: "Tr103",
+            email: "training103@gmail.com",
+            password: "$2b$10$Gwu4BWavhNVbsB7viAiuv.MYKu2/vNIH0SbiqDjYCH00JoIIDL1c6",
+            role: "admin",
+            isVerified: true,
+            userInfo: {
+                urn: 1111114,
+                branch: "Computer Science & Engineering"
+            },
+            tr101: { technology: [], lock: false },
+            tr102: { technology: [], lock: false },
+            tr103: { technology: [], lock: false },
+            tr104: { technology: [], lock: false },
+            placementData: { lock: false }
+        });
+        await user4.save();
+        const user5 = new SignUp({
+            crn: "Tr104",
+            email: "training104@gmail.com",
+            password: "$2b$10$TQC/NMdX9JC4tpXJI68MnOk8VH/oevxmAU.sjB56ki/hh4QPLfib2",
+            role: "admin",
+            isVerified: true,
+            userInfo: {
+                urn: 1111115,
+                branch: "Computer Science & Engineering"
+            },
+            tr101: { technology: [], lock: false },
+            tr102: { technology: [], lock: false },
+            tr103: { technology: [], lock: false },
+            tr104: { technology: [], lock: false },
+            placementData: { lock: false }
+        });
+        await user5.save();
+
+        console.log("Initialized users collection with a document");
+    } catch (error) {
+        console.error("Error initializing users collection:", error);
+        throw error;
+    }
+}
+
+module.exports = { db };
--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const connectToMongo = require('./db');
-
+const connectToMongo = require('./db').db;
 const app = express();
 app.use(cors());
 // Connect to MongoDB
@@ -21,24 +20,24 @@ const userRoute = require('./routes/getUser');
 const validateRoute = require('./routes/Authentication/verify');
 const passwordResetRoute = require('./routes/password/resetPassword');
 const startRoute = require('./routes/wakingServer');
-const tr101=require('./routes/UserProfileData/Training1')
-const tr102=require('./routes/UserProfileData/Training2')
-const tr103=require('./routes/UserProfileData/Training3')
-const tr104=require('./routes/UserProfileData/Training4')
-const placementData=require('./routes/UserProfileData/PlacementData')
-const adminControl=require('./routes/adminControlRoutes/adminControl')
+const tr101 = require('./routes/UserProfileData/Training1')
+const tr102 = require('./routes/UserProfileData/Training2')
+const tr103 = require('./routes/UserProfileData/Training3')
+const tr104 = require('./routes/UserProfileData/Training4')
+const placementData = require('./routes/UserProfileData/PlacementData')
+const adminControl = require('./routes/adminControlRoutes/adminControl')
 
 app.use('/api/userprofiles', userProfileRoutes);
-app.use('/api/tr101',tr101)
-app.use('/api/tr102',tr102)
-app.use('/api/tr103',tr103)
-app.use('/api/tr104',tr104)
-app.use('/api/placement',placementData)
+app.use('/api/tr101', tr101)
+app.use('/api/tr102', tr102)
+app.use('/api/tr103', tr103)
+app.use('/api/tr104', tr104)
+app.use('/api/placement', placementData)
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/validate', validateRoute);
 app.use('/api/password', passwordResetRoute);
-app.use('/api/admin',adminControl)
+app.use('/api/admin', adminControl)
 // Start the server
 const port = process.env.PORT;
 app.listen(port, () => {
